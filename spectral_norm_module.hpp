34264b038f4715178d0a476f82fa52da2109e293
Firstbober <firstbober@tutanota.com>
2026-02-11 20:58:14 +0100

    chore: remove accidentally committed test output files

--- spectral_norm_module.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "l2c_runtime.hpp"
-#include "spectral_norm_state.hpp"
-
-luaValue _l2c__spectral_norm_export(spectral_norm_lua_State* state);
-        
--- test_no_header_module.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "l2c_runtime.hpp"
-#include "test_no_header_state.hpp"
-
-luaValue _l2c__test_no_header_export(test_no_header_lua_State* state);
-        
